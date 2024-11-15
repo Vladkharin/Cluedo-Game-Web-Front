024934cf15455d4efc33bdc3b39d894b6617b4a6
vlad <kooooenienko@gmail.com>
2024-11-16 00:36:23 +0500

    start project

---
+++ src/components/startPage/StartPage.tsx
@@ -0,0 +1,7 @@
+export function StartPage() {
+  return (
+    <section className="w-screen h-screen bg-startPageBG bg-no-repeat bg-center bg-cover">
+      <div className="flex flex-col justify-center items-center"></div>
+    </section>
+  );
+}
